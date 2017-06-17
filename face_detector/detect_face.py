f9701c2521cff9f0af813b52a1abcd19ff781e45
Jan Zikes <zikes@spaceknow.com>
2017-06-17 17:20:26 +0200

    commit initial version

---
+++ face_detector/detect_face.py
@@ -0,0 +1,88 @@
+import argparse
+
+import cv2
+import matplotlib.pylab as plt
+import numpy as np
+
+
+CASCADE_FILE_PATH = "haarcascade_frontalface_default.xml"
+
+
+def parse_args():
+    """
+    Argument parser.
+    """
+    parser = argparse.ArgumentParser('Path to the input image')
+    parser.add_argument('--input_image', type=str, required=True,
+                        help='Path to input image')
+    return parser.parse_args()
+
+
+def locate_faces(input_image):
+    """
+    Locate faces in the original image.
+
+    :param input_image: Array representing the input image.
+    :type input_image: np.ndarray
+    :return: (x, y, w, h)
+    :rtype: (int, int, int, int)
+    """
+    face_cascade = cv2.CascadeClassifier(CASCADE_FILE_PATH)
+    gray = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+    # detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.2, 5)
+    return faces
+
+
+def plot_located_face(image, face_locations):
+    """
+
+    :param image:
+    :type image:
+    :param face_locations:
+    :type face_locations:
+    :return:
+    :rtype:
+    """
+    for (x, y, w, h) in face_locations:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    plt.imshow(image)
+    plt.show()
+
+
+def crop_image(image, face_location):
+    """
+
+    :param image:
+    :type image:
+    :param face_location:
+    :type face_location:
+    :return:
+    :rtype:
+    """
+    croped_image = image.crop(face_location).resize((224, 224))
+    return croped_image
+
+
+def main():
+    args = parse_args()
+    input_image_path = args.input_image
+
+    image = cv2.imread(input_image_path)
+    face_locations = locate_faces(image)
+
+    # optional
+    plot_located_face(image, face_locations)
+
+    # TODO crop the image
+    cropped_images = []
+    for face in face_locations:
+        cropped_images.append(image, face)
+
+    for crop in cropped_images:
+        plt.imshow(np.asarray(crop))
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
